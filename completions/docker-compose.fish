423ecbfd59b512142b2dfed860a5a24ef0da6d3d
Daniel Bergmann <brgmnn@users.noreply.github.com>
2018-01-11 21:41:32 -0800

    Add base docker-compose flags completions (#12)
    Closes #11

--- completions/docker-compose.fish
+++ completions/docker-compose.fish
@@ -59,6 +59,20 @@ end
 # Delete old completions
 complete -c docker-compose -e
 
+# Base docker-compose flags
+complete -c docker-compose -s f -l file -r         -d "Specify an alternate compose file"
+complete -c docker-compose -s p -l project-name -x -d "Specify an alternate project name"
+complete -c docker-compose -l verbose              -d "Show more output"
+complete -c docker-compose -s H -l host -x         -d "Daemon socket to connect to"
+complete -c docker-compose -l tls                  -d "Use TLS; implied by --tlsverify"
+complete -c docker-compose -l tlscacert -r         -d "Trust certs signed only by this CA"
+complete -c docker-compose -l tlscert -r           -d "Path to TLS certificate file"
+complete -c docker-compose -l tlskey -r            -d "Path to TLS key file"
+complete -c docker-compose -l tlsverify            -d "Use TLS and verify the remote"
+complete -c docker-compose -l skip-hostname-check  -d "Don't check the daemon's hostname against the name specified in the client certificate (for example if your docker host is an IP address)"
+complete -c docker-compose -s h -l help            -d "Print usage"
+complete -c docker-compose -s v -l version         -d "Print version and exit"
+
 # All docker-compose commands
 complete -c docker-compose -n '__fish_use_subcommand' -xa build   --description "Build or rebuild services"
 complete -c docker-compose -n '__fish_use_subcommand' -xa bundle  --description "Generate a Docker bundle from the Compose file"
